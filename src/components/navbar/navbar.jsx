cf06e2c12ac314f8ab0ce649e8b18c83a442dc4e
justinait <justiiturraspe@gmail.com>
2023-08-25 20:11:47 -0300

    services first section

--- src/components/navbar/navbar.jsx
+++ src/components/navbar/navbar.jsx
@@ -69,7 +69,7 @@ const NavBar = () => {
                   className='nav-link'
                   smooth="true"
                   duration={500}
-                  to="/Services"
+                  to="/servicios"
                   exact="true"
 
                 >
