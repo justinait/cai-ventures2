58477a243e3df1bb7b4a1270ee6e7d32132946e2
Belen Caporale <caporalebelen14@gmail.com>
2023-08-25 18:22:40 -0300

    information first prt

--- src/components/CardService/Service2.jsx
+++ src/components/CardService/Service2.jsx
@@ -3,11 +3,25 @@ import './Service2.css';
 
 const ServiceDos = () => {
     return (
-        <Container>
+        <Container className='containerServiceDos'>
             <Row className='ServiceContainerRow'>
-                <Col>
-                    <h2 className='tituloSecDos'>¿Qué incluyen las categorías?</h2>
+                <Col className='containerTitle'>
+                    <h2 className='tituloSecDos'>¿Qué incluyen las asesorías?</h2>
                 </Col>
+                <Row>
+                <Col>
+                    <h3 className='subtitleServiceDos'>Asesoría integral</h3>
+                    <Col>
+                        <p>Te apoyamos en cada paso, desde la elección del título de tu tesis hasta su finalización. Nuestro objetivo es ayudarte a superar con éxito el proceso de titulación.</p>
+                    </Col>
+                </Col>
+                <Col>
+                    <h3 className='subtitleServiceDos'>Asesores profesionales</h3>
+                    <Col>
+                        <p>Nuestro equipo de asesoramiento tiene una amplia experiencia y especialización en diversas áreas. Te proporcionaremos una asesoría personalizada según tus necesidades e intereses.</p>
+                    </Col>
+                </Col>
+                </Row>
             </Row>
         </Container>
     )
