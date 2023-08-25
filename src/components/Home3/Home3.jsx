4eee6d234c03e9848331ffbb6f5e33659bf8fde1
Belen Caporale <caporalebelen14@gmail.com>
2023-08-25 10:13:23 -0300

    autoplay

--- src/components/Home3/Home3.jsx
+++ src/components/Home3/Home3.jsx
@@ -1,39 +1,22 @@
+import { Swiper, SwiperSlide } from 'swiper/react';
+import 'swiper/css';
+import 'swiper/css/bundle';
+import 'swiper/css/navigation';
+import 'swiper/css/pagination';
 
-// // import home4 from '../../assets/home4.jpg';
-// import swipperDos from '../../assets/swipperDos.png';
-// import swipperTres from '../../assets/swipperTres.png';
-// import { Swiper, SwiperSlide } from 'swiper/react';
-// import 'swiper/css';
-// import 'swiper/css/bundle';
-// import 'swiper/css/navigation';
-// import 'swiper/css/pagination';
+function Home3() {
+  return (
+    <Swiper
+      className='swiperContainer'
+      spaceBetween={30}
+      pagination={{ clickable: true }}
+      autoplay={{ delay: 1000 }} 
+    >
+      <SwiperSlide>Slide 1</SwiperSlide>
+      <SwiperSlide>Slide 2</SwiperSlide>
+      <SwiperSlide>Slide 3</SwiperSlide>
+    </Swiper>
+  );
+}
 
-// function Home3() {
-//   return (
-//     <Swiper className='carouselHome'
-//         // modules={[Navigation, Pagination, Scrollbar, A11y]}
-//         spaceBetween={50}
-//         slidesPerView={1}
-//         // navigation
-//         // pagination={{ clickable: true }}
-//         // scrollbar={{ draggable: true }}
-//         onSlideChange={() => console.log('slide change')}
-//         onSwiper={(swiper) => console.log(swiper)}
-//         >
-//         <SwiperSlide
-          
-//         >
-//             <img src={swipperDos}/>
-//         </SwiperSlide>
-//         <SwiperSlide>
-//             <img src={swipperDos}/>
-//         </SwiperSlide>
-//         <SwiperSlide>
-//             <img src={swipperTres}/>
-//         </SwiperSlide>
-
-//     </Swiper>
-//   )
-// }
-
-// export default Home3;
+export default Home3;
