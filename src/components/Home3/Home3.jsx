5ab1fead153a3e27f5f487099d6bb9200b832de3
Belen Caporale <caporalebelen14@gmail.com>
2023-08-25 11:16:49 -0300

    swiper autoplay

--- src/components/Home3/Home3.jsx
+++ src/components/Home3/Home3.jsx
@@ -1,22 +1,47 @@
 import { Swiper, SwiperSlide } from 'swiper/react';
+import home4 from '../../assets/home4.jpg';
+import swipperDos from '../../assets/swipperDos.png';
+import swipperTres from '../../assets/swipperTres.png';
 import 'swiper/css';
 import 'swiper/css/bundle';
 import 'swiper/css/navigation';
 import 'swiper/css/pagination';
+import './Home3.css';
 
-function Home3() {
+import React, { useRef, useState } from 'react';
+import {Autoplay, Pagination, Navigation} from 'swiper/modules';
+
+const Home3 = () => {
   return (
-    <Swiper
-      className='swiperContainer'
-      spaceBetween={30}
-      pagination={{ clickable: true }}
-      autoplay={{ delay: 1000 }} 
-    >
-      <SwiperSlide>Slide 1</SwiperSlide>
-      <SwiperSlide>Slide 2</SwiperSlide>
-      <SwiperSlide>Slide 3</SwiperSlide>
+    <Swiper className='carouselHome'
+        centeredSlides={true}
+        autoplay={{
+            delay: 2500,
+            disableOnInteraction: false,
+        }}
+        pagination={{
+            clickable: true,
+          }}
+        spaceBetween={30}
+        slidesPerView={1}
+        navigation={true}
+        modules={[Autoplay, Pagination, Navigation]}
+
+        onSlideChange={() => console.log('slide change')}
+        onSwiper={(swiper) => console.log(swiper)}
+        >
+    <SwiperSlide>
+        <img src={home4} />
+    </SwiperSlide>
+    <SwiperSlide>
+        <img src={swipperDos} />
+    </SwiperSlide>
+    <SwiperSlide>
+        <img src={swipperTres} />
+    </SwiperSlide>
+
     </Swiper>
-  );
+  )
 }
 
 export default Home3;
