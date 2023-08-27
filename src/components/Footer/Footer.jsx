a98bbb2b8f0fa14f07993090b209077c15b30540
Eduardo Tolaba <eduardoetolaba@gmail.com>
2023-08-27 10:33:59 -0300

    changes styles and adaptation full responsive in the component footer

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,4 +1,3 @@
-import React from 'react';
 import './Footer.css';
 import LogoFooter from '../../assets/caiVenturesLogo.png';
 import Instagram from '../../assets/Instagram.png';
@@ -8,36 +7,50 @@ import Linkedin from '../../assets/LinkedIN.png';
 
 const Footer = () => {
     return (
-        <footer className='footer'>
-            <div className='containerLogos'>
-                <img src={LogoFooter} alt="logo de la empresa"/>
-                <ul className='list listContact'>
-                    <li><a href="/"><img src={Instagram} alt="logo de instagram" /></a></li>
-                    <li><a href="/"><img src={Facebook} alt="logo de facebook" /></a></li>
-                    <li><a href="/"><img src={Linkedin} alt="logo de linkedin" /></a></li>
-                    <li><a href="/"><img src={Tiktok} alt="logo de tik tok" /></a></li>
+        <>
+            <footer className='footer'>
+                <div className='containerLogos'>
+                    <img src={LogoFooter} alt="logo de la empresa"/>
+                    <ul className='list listContact'>
+                        <li><a href="/"><img src={Instagram} alt="logo de instagram" /></a></li>
+                        <li><a href="/"><img src={Facebook} alt="logo de facebook" /></a></li>
+                        <li><a href="/"><img src={Linkedin} alt="logo de linkedin" /></a></li>
+                        <li><a href="/"><img src={Tiktok} alt="logo de tik tok" /></a></li>
+                    </ul>
+                </div>
+                <div className='containerIndice'>
+                    <h4>Indice</h4>
+                    <ul className='list listAnimation'>
+                        <li><a href="/">Inicio</a></li>
+                        <li><a href="/about">Nosotros</a></li>
+                        <li><a href="/services">Servicios</a></li>
+                        <li><a href="/contact">Contacto</a></li>
+                        <li><a href="/faq">FAQ</a></li>
+                    </ul>
+                </div>
+                <div className='containerHelp'>
+                    <h4>Ayuda</h4>
+                    <ul className='list listAnimation'>
+                        <li><a href="/">Politicas de Privacidad</a></li>
+                        <li><a href="/">Términos y Condiciones</a></li>
+                    </ul>
+                </div>
+            </footer>
+            <footer className='mobileFooterContainer'>
+                <ul className='list listMobile'>
+                    <li><a href="/"><img className='mobileImagesFooter' src={Instagram} alt="logo de instagram" /></a></li>
+                    <li><a href="/"><img className='mobileImagesFooter' src={Facebook} alt="logo de facebook" /></a></li>
+                    <li><a href="/"><img className='mobileImagesFooter logoImageMobile' src={LogoFooter} alt="logo de la empresa"/></a></li>
+                    <li><a href="/"><img className='mobileImagesFooter' src={Linkedin} alt="logo de linkedin" /></a></li>
+                    <li><a href="/"><img className='mobileImagesFooter' src={Tiktok} alt="logo de tik tok" /></a></li>
                 </ul>
-            </div>
-            <div>
-                <h4>Indice</h4>
-                <ul className='list listAnimation'>
-                    <li><a href="/">Inicio</a></li>
-                    <li><a href="/about">Nosotros</a></li>
-                    <li><a href="/services">Servicios</a></li>
-                    <li><a href="/contact">Contacto</a></li>
-                    <li><a href="/faq">FAQ</a></li>
-                    <li><a href="/">Términos y Condiciones</a></li>
-                </ul>
-            </div>
-            <div className='containerHelp'>
-                <h4>Ayuda</h4>
-                <ul className='list listAnimation'>
-                    <li><a href="/">Preguntas Frecuentes</a></li>
-                    <li><a href="/">Politicas de Privacidad</a></li>
-                    <li><a href="/">Términos y Condiciones</a></li>
-                </ul>
-            </div>
-        </footer>
+                <div className='mobileInfoFooter'>
+                    <a href="/">Términos y Condiciones</a>
+                    <span>-</span>
+                    <a href="/">Politicas de Privacidad</a>
+                </div>
+            </footer>
+        </>
     );
 }
 
