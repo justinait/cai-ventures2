9316166efd3960e395beb58aa2a3748f0d35782f
justinait <justiiturraspe@gmail.com>
2023-08-29 12:54:09 -0300

    alt img

--- src/components/Home2/Home2.jsx
+++ src/components/Home2/Home2.jsx
@@ -5,17 +5,17 @@ import home2 from '../../assets/home22.jpg';
 function Home2() {
   return (
     <div className='homeContainer3'>
-        <img className='homeImageWho' width={200} src={home2} />
-        <div className='homeWho'>
-            <h3 className='homeWhoTitle'>¿Por qué asesorarte con CAI Ventures?</h3>
-            <div className='homeWhoText'>
-                <p> 1. Ahorra tiempo en la elaboración de tu tesis. Con nuestra asesoría y metodología, podrás finalizar tu tesis en menos tiempo y abrirte a mejores oportunidades laborales. </p>
-                <p> 2. Te acompañamos hasta que obtengas tu título. Nuestro equipo de asesores e investigadores está preparado para asegurar que obtengas tu título en el primer intento.</p>
-                <p> 3. Recibe una guía fácil y práctica para avanzar. Gracias a la metodología de nuestros expertos y nuestra plataforma, llevarás un proceso práctico en cada una de las etapas de la asesoría.</p>
-            </div>
-            <Button className='homeButtons' variant="danger">VER MÁS</Button>{' '}
-
+      <img className='homeImageWho' width={200} src={home2} alt='Chica estudiando'/>
+      <div className='homeWho'>
+        <h3 className='homeWhoTitle'>¿Por qué asesorarte con CAI Ventures?</h3>
+        <div className='homeWhoText'>
+          <p> 1. Ahorra tiempo en la elaboración de tu tesis. Con nuestra asesoría y metodología, podrás finalizar tu tesis en menos tiempo y abrirte a mejores oportunidades laborales. </p>
+          <p> 2. Te acompañamos hasta que obtengas tu título. Nuestro equipo de asesores e investigadores está preparado para asegurar que obtengas tu título en el primer intento.</p>
+          <p> 3. Recibe una guía fácil y práctica para avanzar. Gracias a la metodología de nuestros expertos y nuestra plataforma, llevarás un proceso práctico en cada una de las etapas de la asesoría.</p>
         </div>
+        <Button className='homeButtons' variant="danger">VER MÁS</Button>{' '}
+
+      </div>
     </div>
   )
 }
