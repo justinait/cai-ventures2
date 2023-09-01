9d30dd16684aa1b92a5ffea57f6c0daa232be66d
Justina Iturraspe <81998551+justinait@users.noreply.github.com>
2023-09-01 14:58:12 -0300

    Update About.jsx

--- src/page/About/About.jsx
+++ src/page/About/About.jsx
@@ -1,6 +1,5 @@
 import React from 'react'
 import Nosotros1 from '../../components/cardNosotros/Nosotros1'
-import React from 'react';
 import Mision from '../../components/Mision/Mision';
 import Team from '../../components/Team/Team';
 
