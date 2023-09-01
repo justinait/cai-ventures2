3d833c5dd7474b204e6c0f044dfb6faa08b82c99
Belen Caporale <caporalebelen14@gmail.com>
2023-09-01 14:15:17 -0300

    icons

--- src/components/Redes/Redes.jsx
+++ src/components/Redes/Redes.jsx
@@ -14,16 +14,16 @@ const RedesFlotantes = () => {
                 <Col className='d-flex justify-content-end position-fixed bottom-1 end-0 m-4 redesContainer'>
                     <Col className='contRedesRed'>
                     <NavLink target="_blank" to="https://instagram.com/tesiscaiventures?igshid=MzRlODBiNWFlZA==" className="redesIcon">
-                        <Image src={instaAzul} alt="Instagram" />
+                        <Image className='iconImg' src={instaAzul} alt="Instagram" />
                     </NavLink>
                     <NavLink target="_blanck" to="https://www.facebook.com/profile.php?id=100091551814355&mibextid=LQQJ4d" className="redesIcon">
-                        <Image src={faceAzul} alt="Facebook" />
+                        <Image  src={faceAzul} alt="Facebook" />
                     </NavLink>
                     <NavLink target="_blanck" to="https://www.linkedin.com/in/cai-ventures-3033b8271" className="redesIcon">
-                        <Image src={linkAzul} alt="LinkedIn" />
+                        <Image className='iconImg' src={linkAzul} alt="LinkedIn" />
                     </NavLink>
                     <NavLink target="_blanck" to="https://www.tiktok.com/@cai.ventures?_t=8fIegwJ6QUP&_r=1" className="redesIcon">
-                        <Image src={tikAzul} alt="TikTok" />
+                        <Image className='iconImg' src={tikAzul} alt="TikTok" />
                     </NavLink>
                     <NavLink target="_blanck" to="https://wa.link/xn96ef"  className="whatsBlanco">
                         <Image src={whatsBlanco} alt="whatsapp" className="redesIcon whatsappImg" />
