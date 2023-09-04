17b44205059d0f16661d4c9dacb29bc9b0c669cc
Eduardo Tolaba <eduardoetolaba@gmail.com>
2023-09-04 10:41:22 -0300

    added the last card in the about section, with full responsive adaptation

--- src/page/About/About.jsx
+++ src/page/About/About.jsx
@@ -2,15 +2,16 @@ import React from 'react'
 import Nosotros1 from '../../components/cardNosotros/Nosotros1'
 import Mision from '../../components/Mision/Mision';
 import Team from '../../components/Team/Team';
+import Valores from '../../components/valores/Valores';
+
 
 function About() {
   return (
     <div>
-
       <Nosotros1/>
       <Team />
       <Mision />
-        
+      <Valores/>
     </div>
   )
 }
