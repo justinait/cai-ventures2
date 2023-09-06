16c1194a10e900a410e45a279aaab0177f52f595
Belen Caporale <caporalebelen14@gmail.com>
2023-09-06 07:04:21 -0300

    funcionamiento botones

--- src/components/Pagos/Pagos.jsx
+++ src/components/Pagos/Pagos.jsx
@@ -1,26 +1,93 @@
-import Tab from 'react-bootstrap/Tab';
-import Tabs from 'react-bootstrap/Tabs';
+import React, { useState } from 'react';
 import './Pagos.css';
 import Plin from '../../assets/plin.png';
 import Yape from '../../assets/yape.png';
-import { Image } from 'react-bootstrap';
+import tukuy from '../../assets/tukuy.png';
+import code from '../../assets/code.svg';
+import efectivo from '../../assets/efectivo.png';
+import bancoNacion from '../../assets/bancoNacion.png';
+import interBank from '../../assets/interBank.png';
+import bcp from '../../assets/bcp.png';
+import bbva from '../../assets/bbva.png';
+import { Container, Image, Col, Row } from 'react-bootstrap';
 
 function UncontrolledExample() {
+  const [mostrarImagenesOnline, setMostrarImagenesOnline] = useState(false);
+  const [mostrarImagenesEfectivo, setMostrarImagenesEfectivo] = useState(false);
+
+  const toggleImagenesOnline = () => {
+    setMostrarImagenesOnline(!mostrarImagenesOnline);
+  };
+
+  const toggleImagenesEfectivo = () => {
+    setMostrarImagenesEfectivo(!mostrarImagenesEfectivo);
+  };
+
   return (
-    <Tabs
-      defaultActiveKey="profile"
-      id="uncontrolled-tab-example"
-      className="mb-3"
-    >
-      <Tab eventKey="online" title='son online' className='online'>
-        Online
-        <Image src={Plin} />
-        <Image src={Yape} />
-      </Tab>
-      <Tab eventKey="efectivo" title="Efectivo">
-        Efectivo
-      </Tab>
-    </Tabs>
+    <Container className='containerPagos'>
+      <Row>
+        <Col>
+          <button
+            className={`buttonOnline ${mostrarImagenesOnline ? 'active' : ''}`}
+            onClick={toggleImagenesOnline}
+          >
+            <Image src={code} />
+            <p className={`titleOnline ${mostrarImagenesOnline ? 'active-text' : ''}`}>
+              Pago online
+            </p>
+          </button>
+          {mostrarImagenesOnline && (
+            <>
+            <Col>
+                <Image src={Plin} />
+                <p>plin.pe</p>
+            </Col>
+            <Col>
+                <Image src={Yape} />
+                <p>yape.com.pe</p>
+            </Col>
+            <Col>
+                <Image src={tukuy} />
+                <p>tukuy.club</p>
+            </Col>
+            </>
+          )}
+        </Col>
+      </Row>
+      <Row>
+        <Col>
+          <button
+            className={`buttonEfectivo ${mostrarImagenesEfectivo ? 'active' : ''}`}
+            onClick={toggleImagenesEfectivo}
+          >
+            <Image src={efectivo} />
+            <p className={`titleEfectivo ${mostrarImagenesEfectivo ? 'active-text' : ''}`}>
+              Pago en efectivo
+            </p>
+          </button>
+          {mostrarImagenesEfectivo && (
+            <>
+            <Col>
+                <Image src={bancoNacion} />
+                <p>www.bn.com.pe</p>
+            </Col>
+            <Col>
+                <Image src={interBank} />
+                <p>interbank.pe</p>
+            </Col>
+            <Col>
+                <Image src={bcp} />
+                <p>www.viabcp.pe</p>
+            </Col>
+            <Col>
+                <Image src={bbva} />
+                <p>www.bbva.pe</p>
+            </Col>
+            </>
+          )}
+        </Col>
+      </Row>
+    </Container>
   );
 }
 
