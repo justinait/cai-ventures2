4be297fb584426ee06645f71c89c8e01568ca558
justinait <justiiturraspe@gmail.com>
2023-09-07 12:09:35 -0300

    logica funcional blog

--- src/components/Article/Article.jsx
+++ src/components/Article/Article.jsx
@@ -1,9 +1,43 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react';
+import { useParams } from 'react-router-dom';
 
-function Article() {
+function Article() { 
+
+  const [data, setData] = useState([]);
+  const [thisArticle, setThisArticle] = useState({})
+
+  const {id} = useParams();
+
+  useEffect(() => {
+    fetch('/data/blog.json')
+      .then((response) => response.json())
+      .then((responseData) => {
+        
+        setData(responseData.posts);
+        setThisArticle(responseData.posts[id - 1]);
+      
+      });
+  }, [id]);
+
+  const {title, subtitle, image, content, author} = thisArticle;
+
   return (
     <div>
-        
+      <div className='card2blog'>
+        <div className='card2blogimg'>
+          <img src={image}   />
+        </div>
+        <div className='card2blogtext'>
+
+          <div className='text-section'>
+            <p className='text-sectionSubtitle'>{subtitle}</p>
+            <p className='text-sectionTitle'>{title}</p>
+            <p className='text-sectionDescription'>{content}</p>
+          </div>
+
+        </div>
+
+      </div>
     </div>
   )
 }
--- src/page/Blog/Blog.jsx
+++ src/page/Blog/Blog.jsx
@@ -5,20 +5,17 @@ import img1blog from '../../assets/img1blog.png'
 import img2blog from '../../assets/Img2blog.png'
 import img3blog from '../../assets/Img3blog.png'
 import buttonshareblog from '../../assets/buttonshareblog.png'
+import { Link } from 'react-router-dom';
 
 const Blog = () => {
 
     const [data, setData] = useState([]);
 
     useEffect(() => {
-        fetch('../../../public/data/blog.json')
+        fetch('/data/blog.json')
         .then((response) => response.json())
         .then((data) => setData(data.posts));
     }, []);
-
-    if (!data) {
-        return <div>Cargando...</div>;
-    }
 
     return (
 
@@ -39,29 +36,34 @@ const Blog = () => {
 
             <div className='segondcardblog'>
                 {data.map(e => {
-                    console.log(e);
+                    
+                    const {title, subtitle, image, content, author} = e;
+                    
                     return (
-                        <div className='card2blog'>
-                            <div className='card2blogimg'>
-                                <img src={e.image} alt={e.title} />
-                            </div>
-                            <div className='card2blogtext'>
+                        <Link to={`/blog/${e.id}`} key={e.id}>
+                            <div className='card2blog'>
+                                <div className='card2blogimg'>
+                                    <img src={image} alt={title} />
+                                </div>
+                                <div className='card2blogtext'>
 
 
-                                <div className='text-section'>
-                                    <p className='text-sectionSubtitle'>{e.subtitle}</p>
-                                    <p className='text-sectionTitle'>{e.title}</p>
-                                    <p className='text-sectionDescription'>{e.content}</p>
+                                    <div className='text-section'>
+                                        <p className='text-sectionSubtitle'>{subtitle}</p>
+                                        <p className='text-sectionTitle'>{title}</p>
+                                        <p className='text-sectionDescription'>{content}</p>
+                                    </div>
+
+                                    <button className='buttonshareblog'>
+                                        <img src={buttonshareblog} alt="" />
+                                    </button>
+
                                 </div>
 
-                                <button className='buttonshareblog'>
-                                    <img src={buttonshareblog} alt="" />
-                                </button>
-
                             </div>
 
-                        </div>
-
+                        </Link>
+                        
                     )
                     
                 })}
