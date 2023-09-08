90d7dcb919c27af4d9655fd6acea68c21a6a85e0
Eduardo Tolaba <eduardoetolaba@gmail.com>
2023-09-08 12:47:20 -0300

    arreglo, en el home

--- src/components/cardHome/Card1/Card1.jsx
+++ src/components/cardHome/Card1/Card1.jsx
@@ -8,9 +8,9 @@ const Card1 = () => {
             <div className='homeTesisText'>
                 <h3>Asesoría de tesis desde cero hasta su preparación para la sustentación</h3>
                 <p>Nuestro personal de asesorias e investigación te acompañará en todas las etapas del proceso de asesoria de tesis:</p>
-                <p>-Plan de Tesis <br />
-                -Borrador de Tesis <br />
-                -Sustentación
+                <p>. Plan de Tesis <br />
+                . Borrador de Tesis <br />
+                . Sustentación
                 </p>
                 <img src={cardImage1} className='imgHomeTablet' alt="chica sentada leyendo un libro" />
                 <span><Link className='linkAboutTesis' to='/services'>ver más</Link></span>
