0dd986541de03057e73ea24e9860a2a3739a6f5c
Belen Caporale <caporalebelen14@gmail.com>
2023-09-09 13:35:59 -0300

    section payment

--- src/components/Pagos/Pagos.jsx
+++ src/components/Pagos/Pagos.jsx
@@ -9,38 +9,111 @@ import bancoNacion from '../../assets/bancoNacion.png';
 import interBank from '../../assets/interBank.png';
 import bcp from '../../assets/bcp.png';
 import bbva from '../../assets/bbva.png';
-import { Link } from 'react-router-dom';
-
 import { Container, Image, Col, Row } from 'react-bootstrap';
+import YapeScreen from './Pantallas/Yape';
+import TukuyScreen from './Pantallas/Tukuy';
+import PlinScreen from './Pantallas/Plin';
+import BancoNacion from './Efectivo/BancoNacion'; 
+import flechaPagos from '../../assets/flechaPagos.svg';
+import InterScreen from './Efectivo/InterBanck';
 
 function Pagos() {
   const [mostrarImagenesOnline, setMostrarImagenesOnline] = useState(false);
   const [mostrarImagenesEfectivo, setMostrarImagenesEfectivo] = useState(false);
+  const [mostrarPlinScreen, setMostrarPlinScreen] = useState(false);
+  const [mostrarYapeScreen, setMostrarYapeScreen] = useState(false);
+  const [mostrarTukuyScreen, setMostrarTukuyScreen] = useState(false);
+  const [mostrarBancoNacionScreen, setMostrarBancoNacionScreen] = useState(false); 
+  const [mostrarInterScreen, setMostrarInterScreen] = useState(false);
 
   const toggleImagenesOnline = () => {
-    setMostrarImagenesOnline(!mostrarImagenesOnline);
+    if (!mostrarBancoNacionScreen && !mostrarPlinScreen && !mostrarTukuyScreen && !mostrarYapeScreen  && !mostrarInterScreen) {
+      setMostrarImagenesOnline(!mostrarImagenesOnline);
+    }
   };
-
-  const toggleImagenesEfectivo = () => {
-    setMostrarImagenesEfectivo(!mostrarImagenesEfectivo);
-  };
-
   
+  const toggleImagenesEfectivo = () => {
+    if (!mostrarBancoNacionScreen && !mostrarPlinScreen && !mostrarTukuyScreen && !mostrarYapeScreen && !mostrarInterScreen) {
+      setMostrarImagenesEfectivo(!mostrarImagenesEfectivo);
+    }
+  };
+
+  const mostrarPlin = () => {
+    setMostrarInterScreen(false);
+    setMostrarPlinScreen(true);
+    setMostrarYapeScreen(false);
+    setMostrarTukuyScreen(false);
+    setMostrarBancoNacionScreen(false); 
+  };
+
+  const mostrarYape = () => {
+    setMostrarInterScreen(false);
+    setMostrarPlinScreen(false);
+    setMostrarYapeScreen(true);
+    setMostrarTukuyScreen(false);
+    setMostrarBancoNacionScreen(false);
+  };
+
+  const mostrarTukuy = () => {
+    setMostrarInterScreen(false);
+    setMostrarPlinScreen(false);
+    setMostrarYapeScreen(false);
+    setMostrarTukuyScreen(true);
+    setMostrarBancoNacionScreen(false); 
+  };
+
+  const mostrarBancoNacion = () => {
+    setMostrarInterScreen(false);
+    setMostrarPlinScreen(false);
+    setMostrarYapeScreen(false);
+    setMostrarTukuyScreen(false);
+    setMostrarBancoNacionScreen(true); 
+    setMostrarImagenesEfectivo(false); 
+  };
+
+  const mostrarInter = () => {
+    setMostrarPlinScreen(false);
+    setMostrarYapeScreen(false);
+    setMostrarTukuyScreen(false);
+    setMostrarBancoNacionScreen(false);
+    setMostrarInterScreen(true); 
+    setMostrarImagenesEfectivo(false); 
+  };
+
+  const reiniciarEstado = () => {
+    setMostrarImagenesOnline(false);
+    setMostrarImagenesEfectivo(false);
+    setMostrarPlinScreen(false);
+
+    setMostrarYapeScreen(false);
+    setMostrarTukuyScreen(false);
+    setMostrarBancoNacionScreen(false); 
+    setMostrarInterScreen(false);
+  };
 
   return (
     <Container className='containerPagos'>
-        <Row>
-            <Col className='containerPay'>
-                <h2 className='titlePay'>Selecciona un método de pago</h2>
-            </Col>
-        </Row>
-        <Row className='containerBox'>
+      <Col>
+        <Image src={flechaPagos} onClick={reiniciarEstado} />
+      </Col>
       <Row>
-        <Col className='containerBtn'>
-            
+        <Col className='containerPay'>
+          <h2 className='titlePay'>Selecciona un método de pago</h2>
+        </Col>
+      </Row>
+      <Row className='containerBox'>
+        <Row>
+          <Col className='containerBtn'>
           <button
             className={`buttonOnline ${mostrarImagenesOnline ? 'active' : ''}`}
             onClick={toggleImagenesOnline}
+            disabled={
+              mostrarBancoNacionScreen ||
+              mostrarPlinScreen ||
+              mostrarTukuyScreen ||
+              mostrarYapeScreen ||
+              mostrarInterScreen
+            } 
           >
             <Image src={code} className={`imageOnline `} />
             <p className={`titleOnline ${mostrarImagenesOnline ? 'active-text' : ''}`}>
@@ -48,94 +121,127 @@ function Pagos() {
             </p>
           </button>
 
-          {mostrarImagenesOnline && (
-            <>
+            {mostrarImagenesOnline && !mostrarPlinScreen && !mostrarYapeScreen && !mostrarTukuyScreen && (
+              <Row className='containerOnlinePay'>
+                <Col>
+                  <button
+                    className={`buttonDosPay ${mostrarPlinScreen ? 'active' : ''}`}
+                    onClick={() => {
+                      mostrarPlin();
+                    }}
+                  >
+                    <Col className='interBtn'>
+                      <Image src={Plin} />
+                      <p>plin.pe</p>
+                    </Col>
+                  </button>
+                </Col>
+                <Col>
+                  <button
+                    className={`buttonDosPay ${mostrarYapeScreen ? 'active' : ''}`}
+                    onClick={() => {
+                      mostrarYape();
+                    }}
+                  >
+                    <Col className='interBtn'>
+                      <Image src={Yape} />
+                      <p>yape.com.pe</p>
+                    </Col>
+                  </button>
+                </Col>
+                <Col>
+                  <button
+                    className={`buttonDosPay ${mostrarTukuyScreen ? 'active' : ''}`}
+                    onClick={() => {
+                      mostrarTukuy();
+                    }}
+                  >
+                    <Col className='interBtn'>
+                      <Image src={tukuy} />
+                      <p>tukuy.club</p>
+                    </Col>
+                  </button>
+                </Col>
+              </Row>
+            )}
+          </Col>
 
-            <Row className='containerOnlinePay'>
-            <Col>
-                <button className='buttonDosPay'>
-                <Col className='interBtn'>
-                    <Image src={Plin} />
-                    <p>plin.pe</p>
-                </Col>
-                </button>
-            </Col>
-            <Col>
-                <button className='buttonDosPay'>
-                <Col className='interBtn'>
-                    <Image src={Yape} />
-                    <p>yape.com.pe</p>
-                </Col>
-                </button>
-            </Col>
-            <Col>
-                <button className='buttonDosPay'>
-                <Col className='interBtn'>
-                    <Image src={tukuy} />
-                    <p>tukuy.club</p>
-                </Col>
-                </button>
-            </Col>
-            </Row>
-            </>
-          )}
-        </Col>
-      
-        <Col className='containerBtn'>
+          <Col className='containerBtn'>
           <button
             className={`buttonEfectivo ${mostrarImagenesEfectivo ? 'active' : ''}`}
             onClick={toggleImagenesEfectivo}
+            disabled={
+              mostrarBancoNacionScreen ||
+              mostrarPlinScreen ||
+              mostrarTukuyScreen ||
+              mostrarYapeScreen ||
+              mostrarInterScreen
+            } 
           >
             <Image src={efectivo} className={`imageEfectivo `} />
             <p className={`titleEfectivo ${mostrarImagenesEfectivo ? 'active-text' : ''}`}>
               Pago en efectivo
             </p>
           </button>
-        
-          {mostrarImagenesEfectivo && (
-            <>
 
-            <Row className='containerOnlinePay'>
-            <Col>
-
-                <button className='buttonDosPay'>
-                    <Col className='interBtn'>
-                    <Image src={bancoNacion} />
-                    <p>www.bn.com.pe</p>
-                    </Col>
-                </button>
-
-            </Col>
-            <Col>
-                <button className='buttonDosPay'>
-                <Col className='interBtn'>
-                <Image src={interBank} />
-                <p>interbank.pe</p>
+            {mostrarImagenesEfectivo && !mostrarPlinScreen && !mostrarYapeScreen && !mostrarTukuyScreen && (
+              <Row className='containerOnlinePay'>
+                <Col>
+                <button
+                className={`buttonDosPay ${mostrarTukuyScreen ? 'active' : ''}`}
+                onClick={() => {
+                  mostrarBancoNacion();
+                }}
+                  
+                    >
+                      <Col className='interBtn'>
+                        <Image src={bancoNacion} />
+                        <p>www.bn.com.pe</p>
+                      </Col>
+                    </button>
                 </Col>
-                </button>
-            </Col>
-            <Col>
-                <button className='buttonDosPay'>
-                    <Col className='interBtn'>
-                    <Image src={bcp} />
-                    <p>www.viabcp.pe</p>
-                    </Col>
-                </button>
-            </Col>
-            <Col>
-                <button className='buttonDosPay'>
-                <Col className='interBtn'>
-                <Image src={bbva} />
-                <p>www.bbva.pe</p>
+                <Col>
+                <button
+                className={`buttonDosPay ${mostrarTukuyScreen ? 'active' : ''}`}
+                onClick={() => {
+                  mostrarInter();
+                }}
+                  
+                    >
+                      <Col className='interBtn'>
+                      <Image src={interBank} />
+                      <p>interbank.pe</p>
+                      </Col>
+                    </button>
+                 
                 </Col>
-                </button>
-            </Col>
-            </Row>
-            </>
-          )}
-       </Col>
-       </Row>
+                <Col>
+                  <button className='buttonDosPay'>
+                    <Col className='interBtn'>
+                      <Image src={bcp} />
+                      <p>www.viabcp.pe</p>
+                    </Col>
+                  </button>
+                </Col>
+                <Col>
+                  <button className='buttonDosPay'>
+                    <Col className='interBtn'>
+                      <Image src={bbva} />
+                      <p>www.bbva.pe</p>
+                    </Col>
+                  </button>
+                </Col>
+              </Row>
+            )}
+          </Col>
+        </Row>
       </Row>
+      
+      {mostrarYapeScreen && <YapeScreen />}
+      {mostrarTukuyScreen && <TukuyScreen />}
+      {mostrarBancoNacionScreen && <BancoNacion />} 
+      {mostrarPlinScreen && <PlinScreen />}
+      {mostrarInterScreen && <InterScreen />}
     </Container>
   );
 }
---
+++ src/components/Pagos/Pantallas/Tukuy.jsx
@@ -0,0 +1,48 @@
+import { Container, Image, Col, Row } from 'react-bootstrap';
+import React from 'react';
+import tukuyPago from '../../../assets/tukuyPago.png';
+import './Yape.css';
+import qrYape from '../../../assets/qrYape.png';
+import linkCopiar from '../../../assets/linkCopiar.png';
+
+function TukuyScreen () { 
+    
+    return (
+        <Container className='containerPagosOk'>
+            <Row className='contenedorTodoPago'>
+                <Col className='datosPago'>
+                    <h2 className='escaneo'>Realiza el pago escaneando el <span className='escaneoBold'>QR</span></h2>
+                </Col>
+                <Row className='contenedorMonto'>
+                    <Col className='montoImgText'>
+                    <p className='monto'>Monto a pagar $100</p>
+                    <Row className='imgQrCont'>
+                        <Col className='contieneImgQr'>
+                        <Image src={qrYape} />
+                        </Col>
+                    </Row>
+                    <Image src={tukuyPago} />
+                    </Col>
+                </Row>
+
+                <Row>
+                    <Col className='contComprobante'>
+                        <p className='textoComprobante'>Por favor, envía el comprobante de pago a:</p>
+                        <p className='comprobanteColor'>955 716 302 <span className='textoComprobante'>o</span>  928 897 150</p>
+
+                       <p className='linkPago'>O puedes utilizar este link de pago</p>
+
+                       <Col className='linkCopiar'>
+                        <p className='textLinkCopiar'>link.pago.pe/12321</p>
+                        <Image src={linkCopiar} />
+                       </Col>
+
+                       <button onClick={''}  className='cancelarPago'>CANCELAR</button>
+                    </Col>
+                </Row>
+            </Row>
+        </Container>
+    )
+}
+
+export default TukuyScreen;
