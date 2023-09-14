fbb97355c120c755b26d708652ab31b23feef2bf
justinait <justiiturraspe@gmail.com>
2023-09-14 14:54:17 -0300

    navbar collapse

--- src/components/navbar/navbar.jsx
+++ src/components/navbar/navbar.jsx
@@ -2,21 +2,14 @@ import { useState } from "react";
 import 'bootstrap/dist/css/bootstrap.min.css';
 import "bootstrap";
 import { NavLink } from 'react-router-dom';
-
 import Navbar from 'react-bootstrap/Navbar';
-
 import { useEffect } from "react";
 import { Image } from 'react-bootstrap';
 import Logo from '../../assets/logoNav.png';
 import logoBajada from '../../assets/logoBajada.png';
 import './navbar.css';
 
-
-
 const NavBar = () => {
-
-
-
   const [collapsed, setCollapsed] = useState(true);
   const [isDesktop, setIsDesktop] = useState(window.innerWidth > 1024);
   const logoToShow = isDesktop ? logoBajada : Logo;
@@ -33,36 +26,52 @@ const NavBar = () => {
     };
   }, []);
 
+  const handleMenuClick = () => {
+    setCollapsed(true);
+  };
+
   return (
-    <nav className="contieneNav navbar navbar-expand-lg navbar-dark ">
+    <nav className="contieneNav navbar navbar-expand-lg navbar-dark">
       <div className="container NavbarCont">
         <Navbar.Brand href="#home">
           <NavLink to="/">
             <Image className='logoNav' src={logoToShow} />
           </NavLink>
         </Navbar.Brand>
-        <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation" onClick={() => setCollapsed(!collapsed)}>
-          {collapsed ? (<svg xmlns="http://www.w3.org/2000/svg" height="2rem" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" >
-            <path strokeLinecap="round" strokeLinejoin="round" d="M3.75 6.75h16.5M3.75 12h16.5m-16.5 5.25h16.5" />
-          </svg>) : (<svg xmlns="http://www.w3.org/2000/svg" fill="none" height="2rem" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor">
-            <path strokeLinecap="round" strokeLinejoin="round" d="M6 18L18 6M6 6l12 12" />
-          </svg>
+        <button
+          className="navbar-toggler"
+          type="button"
+          data-bs-toggle="collapse"
+          data-bs-target="#navbarNav"
+          aria-controls="navbarNav"
+          aria-expanded="false"
+          aria-label="Toggle navigation"
+          onClick={() => setCollapsed(!collapsed)}
+        >
+          {collapsed ? (
+            <svg xmlns="http://www.w3.org/2000/svg" height="2rem" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor">
+              <path strokeLinecap="round" strokeLinejoin="round" d="M3.75 6.75h16.5M3.75 12h16.5m-16.5 5.25h16.5" />
+            </svg>
+          ) : (
+            <svg xmlns="http://www.w3.org/2000/svg" fill="none" height="2rem" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor">
+              <path strokeLinecap="round" strokeLinejoin="round" d="M6 18L18 6M6 6l12 12" />
+            </svg>
           )}
         </button>
-        <div className="collapse navbar-collapse" id="navbarNav">
+        <div className={`collapse navbar-collapse ${collapsed ? '' : 'show'}`} id="navbarNav">
           <ul className="navbar-nav me-auto">
-            <NavLink className='nav-link' to='/'> Inicio </NavLink>
-            <NavLink className='nav-link' to='/services'>Servicios</NavLink>
-            <NavLink className='nav-link' to='/about'>Nosotros</NavLink>
-            <NavLink className='nav-link' to='/blog'>Blog</NavLink>
-            <NavLink className='nav-link' to='/contact'>Contacto</NavLink>
-            <NavLink className='nav-link_button' to='/Pagos'>Contratar</NavLink>
-            <NavLink className='nav-link nav-link_hidden' to='/faq'>FAQ</NavLink>
+            <NavLink className='nav-link' to='/' onClick={handleMenuClick}> Inicio </NavLink>
+            <NavLink className='nav-link' to='/services' onClick={handleMenuClick}>Servicios</NavLink>
+            <NavLink className='nav-link' to='/about' onClick={handleMenuClick}>Nosotros</NavLink>
+            <NavLink className='nav-link' to='/blog' onClick={handleMenuClick}>Blog</NavLink>
+            <NavLink className='nav-link' to='/contact' onClick={handleMenuClick}>Contacto</NavLink>
+            <NavLink className='nav-link_button' to='/Pagos' onClick={handleMenuClick}>Contratar</NavLink>
+            <NavLink className='nav-link nav-link_hidden' to='/faq' onClick={handleMenuClick}>FAQ</NavLink>
           </ul>
         </div>
       </div>
     </nav>
-  )
-}
+  );
+};
 
 export default NavBar;
